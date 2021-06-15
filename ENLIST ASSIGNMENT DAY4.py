fb4d2cfc94091cc36e84c247fbfb52fb1ba0809f
Santhosh <85781259+Santhosh02K@users.noreply.github.com>
2021-06-15 18:59:20 +0530

    Add files via upload

---
+++ ENLIST ASSIGNMENT DAY4.py
@@ -0,0 +1,17 @@
+#Exercise
+a = b = c = 60
+print(a,b,c)
+print(a/10)
+print(b*50)
+print(c+60)
+
+#create tow values (a,b) of int,float data type and convert the versa
+var_1 = "abcde"
+x = var_1.replace(var_1[2],"G")
+print(x)
+
+#create two values (a,b) of int,float data type & convert the vise versa
+a,b = 35.5,50.4
+a1 = (float(a))
+b1 = (float(b))
+print(a1,b1)
